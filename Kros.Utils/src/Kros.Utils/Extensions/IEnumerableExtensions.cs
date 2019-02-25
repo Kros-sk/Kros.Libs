84831e417bbda622142184594c6bd471029d8e6e
Lukáš Šefčík <lukas.sefcik@gmail.com>
2019-02-25 15:28:26 +0100

    Updated ForEach parameter comment.

--- Kros.Utils/src/Kros.Utils/Extensions/IEnumerableExtensions.cs
+++ Kros.Utils/src/Kros.Utils/Extensions/IEnumerableExtensions.cs
@@ -13,7 +13,7 @@ namespace Kros.Extensions
         /// </summary>
         /// <typeparam name="T">The type of the enumerable.</typeparam>
         /// <param name="source">Input enumerable.</param>
-        /// <param name="action">Action that modifies entries in input enumerable.</param>
+        /// <param name="action">Action that perform <paramref name="action"/> on each element of <paramref name="source"/>.</param>
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
             foreach (T item in source)
