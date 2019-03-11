c741edda31ff18cf796c4c390bdd9f639d8a48ab
Miňo Martiniak <mino.martiniak@gmail.com>
2019-03-11 08:42:19 +0100

    Inject runner into Invoke method

--- Kros.KORM/src/Kros.KORM.Extensions.Asp/Migrations/Middleware/MigrationsMiddleware.cs
+++ Kros.KORM/src/Kros.KORM.Extensions.Asp/Migrations/Middleware/MigrationsMiddleware.cs
@@ -16,38 +16,37 @@ namespace Kros.KORM.Migrations.Middleware
         private readonly RequestDelegate _next;
         private readonly MigrationMiddlewareOptions _options;
         private readonly IMemoryCache _cache;
-        private readonly IMigrationsRunner _migrationsRunner;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MigrationsMiddleware"/> class.
         /// </summary>
         /// <param name="next">The next delegate.</param>
         /// <param name="cache">Memory cache.</param>
-        /// <param name="migrationsRunner">Migrations runner.</param>
         /// <param name="options">Migration options.</param>
         public MigrationsMiddleware(
             RequestDelegate next,
             IMemoryCache cache,
-            IMigrationsRunner migrationsRunner,
             MigrationMiddlewareOptions options)
         {
             _next = next;
             _options = Check.NotNull(options, nameof(options));
             _cache = Check.NotNull(cache, nameof(cache));
-            _migrationsRunner = Check.NotNull(migrationsRunner, nameof(migrationsRunner));
         }
 
         /// <summary>
         /// Invokes the specified context.
         /// </summary>
         /// <param name="context">The context.</param>
-        public async Task Invoke(HttpContext context)
+        /// <param name="migrationsRunner">Migrations runner.</param>
+        public async Task Invoke(
+            HttpContext context,
+            IMigrationsRunner migrationsRunner)
         {
             if (CanMigrate())
             {
                 SetupCache();
 
-                await _migrationsRunner.MigrateAsync();
+                await migrationsRunner.MigrateAsync();
             }
         }
 
